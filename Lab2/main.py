eaf723bade8651b27f728d3a8c7782655dfa9e47
Hubert Kowalczyk <hubertksk@gmail.com>
2021-12-13 21:09:58 +0100

    update

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import csv
-import logging
 import logging.handlers
 import os
 import re
@@ -23,28 +22,27 @@ except Exception as e:
     pass
 
 try:
-    if os.path.exists("Lab2/export/content.csv"):
-        open('Lab2/export/content.csv', 'w').close()
+    if os.path.exists("export/content.csv"):
+        open('export/content.csv', 'w').close()
 except Exception as e:
     pass
 
-
 try:
-    os.mkdir('Lab2/export')
+    os.mkdir('export')
 except OSError as error:
     LOGGER.debug(error)
 
 starting_web_address = 'https://www.techsterowniki.pl/serwis/kontakt-serwis'
 
 
-def scrape_web(web_adress, starting_web_address):
+def scrape_web(web_address, starting_web_address):
     try:
-        r = requests.get(web_adress)
+        r = requests.get(web_address)
     except requests.exceptions.SSLError as e:
         LOGGER.info("SSL Certificate failed")
-        r = requests.get(web_adress, verify=False)
+        r = requests.get(web_address, verify=False)
     except requests.exceptions.MissingSchema as e:
-        r = requests.get(starting_web_address + web_adress)
+        r = requests.get(starting_web_address + web_address)
     except Exception as e:
         raise e
 
@@ -57,7 +55,7 @@ def scrape_web(web_adress, starting_web_address):
 
 
 def ngram(text_dump, n=1):
-    text_dump = re.sub('[^A-Za-z0-9; ążźćńółęąśŻŹĆĄŚĘŁÓŃ]+', '', text_dump)
+    text_dump = re.sub('[^A-Za-z0-9; ążźćńółęśŻŹĆĄŚĘŁÓŃ]+', '', text_dump)
     sentence_list = text_dump.split(';')
     ngrams = set()
     for idx, val in enumerate(sentence_list):
@@ -66,7 +64,6 @@ def ngram(text_dump, n=1):
         temp = zip(*[words[i:] for i in range(0, n)])
         ans = [' '.join(n) for n in temp]
         for idx, val2 in enumerate(ans):
-
             ngrams.add(val2)
 
     return ngrams
@@ -97,20 +94,11 @@ def find_links(soup):
 def extract_text(soup, webpage):
     try:
         text = soup.get_text(separator=';')
-
-        # TODO:
-        for x in range(5):
-            text = text.replace("\r", " ")
-            text = text.replace("\n", " ")
-            text = text.replace("; ;", ";")
-            text = text.replace(" ;", ";")
-            text = text.replace("; ", ";")
-            text = text.replace(";;", ";")
-            text = text.replace("\t", " ")
-            text = text.replace("  ", " ")
+        text = re.sub("\s?;+\s?|\r|\n|\t", ";", text)
+        text = re.sub("\s+", " ", text)
 
         text = text.strip()
-        with open(f'Lab2/export/content.csv', 'a') as f:
+        with open(f'export/content.csv', 'a') as f:
             f.write(webpage + ';')
             f.write(text)
             f.write("\n")
@@ -126,35 +114,35 @@ def find_email(page_content):
     # emails = emails.split()
     if emails:
         for idx, val in enumerate(emails):
-            f = open('Lab2/export/emails.csv', 'a')
+            f = open('export/emails.csv', 'a')
             write = csv.writer(f)
             write.writerow([val])
             f.close()
     return emails
 
 
-def remove_duplicates(emails_file='Lab2/export/emails.csv'):
+def remove_duplicates(emails_file='export/emails.csv'):
     df = pd.read_csv(emails_file, delimiter=',')
     df.drop_duplicates(subset=None, inplace=True)
     df.to_csv(emails_file, index=False)
     # mylist = list(dict.fromkeys(mylist))
 
 
-def main(web_adress):
-    starting_web_address = web_adress
+def main(web_address):
+    starting_web_address = web_address
     visited_adresses = {starting_web_address}
     LOGGER.info("[Starting]")
-    soup = scrape_web(web_adress, starting_web_address)
+    soup = scrape_web(web_address, starting_web_address)
     links = find_links(soup)
 
-    text = extract_text(soup.body, web_adress)
+    text = extract_text(soup.body, web_address)
     ngram_set = ngram(text, 2)
     find_email(text)
 
     LOGGER.info("Initial scraping done. Scraping links")
     for idx, val in enumerate(links):
         if val in visited_adresses:
-            LOGGER.debug("Adress already visited. Skipping.")
+            LOGGER.debug("Address already visited. Skipping.")
             continue
         else:
             visited_adresses.add(val)

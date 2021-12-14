13ffc73ee7f363affd322e6d837903e42380ded6
Hubert Kowalczyk <hubertksk@gmail.com>
2021-12-14 22:52:50 +0100

    base done

--- Lab2/main.py
+++ Lab2/main.py
@@ -20,6 +20,11 @@ try:
         open('scrapper.log', 'w').close()
 except Exception as e:
     pass
+try:
+    if os.path.exists("export/ngrams.csv"):
+        open('export/ngrams.csv', 'w').close()
+except Exception as e:
+    pass
 
 try:
     if os.path.exists("export/content.csv"):
@@ -32,12 +37,10 @@ try:
 except OSError as error:
     LOGGER.debug(error)
 
-starting_web_address = 'https://www.techsterowniki.pl/serwis/kontakt-serwis'
-
 
 def scrape_web(web_address, starting_web_address):
     try:
-        r = requests.get(web_address)
+        r = requests.get(web_address, timeout=3)
     except requests.exceptions.SSLError as e:
         LOGGER.info("SSL Certificate failed")
         r = requests.get(web_address, verify=False)
@@ -54,18 +57,24 @@ def scrape_web(web_address, starting_web_address):
         return BeautifulSoup("BLANK", "lxml")
 
 
-def ngram(text_dump, n=1):
+def ngram(text_dump, webpage, n=1):
     text_dump = re.sub('[^A-Za-z0-9; ążźćńółęśŻŹĆĄŚĘŁÓŃ]+', '', text_dump)
     sentence_list = text_dump.split(';')
     ngrams = set()
     for idx, val in enumerate(sentence_list):
-
         words = [word for word in val.split(" ")]
         temp = zip(*[words[i:] for i in range(0, n)])
         ans = [' '.join(n) for n in temp]
-        for idx, val2 in enumerate(ans):
+        for _, val2 in enumerate(ans):
             ngrams.add(val2)
 
+    with open(f'export/ngrams.csv', 'a') as f:
+        f.write(webpage + ';')
+        for _, val in enumerate(list(ngrams)):
+            f.write(f"{val};")
+
+        f.write("\n")
+        LOGGER.debug(f"[WRITE]{ngrams}")
     return ngrams
 
 
@@ -78,14 +87,12 @@ def find_links(soup):
     regex = r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})'
 
     r = re.compile(regex)
-    # regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
     links_filtered = [i for i in links if i]
     try:
         links_filtered.remove("javascript:void(0)")
     except Exception as e:
         raise e
-
-    # links_filtered = list(filter(r.match, links_filtered))
+    
     LOGGER.info(f"LINKS: {links_filtered}")
 
     return links_filtered
@@ -95,6 +102,7 @@ def extract_text(soup, webpage):
     try:
         text = soup.get_text(separator=';')
         text = re.sub("\s?;+\s?|\r|\n|\t", ";", text)
+        text = re.sub("\s?;+\s?", ";", text)
         text = re.sub("\s+", " ", text)
 
         text = text.strip()
@@ -111,7 +119,6 @@ def extract_text(soup, webpage):
 def find_email(page_content):
     regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
     emails = re.findall(regex, page_content)
-    # emails = emails.split()
     if emails:
         for idx, val in enumerate(emails):
             f = open('export/emails.csv', 'a')
@@ -125,35 +132,89 @@ def remove_duplicates(emails_file='export/emails.csv'):
     df = pd.read_csv(emails_file, delimiter=',')
     df.drop_duplicates(subset=None, inplace=True)
     df.to_csv(emails_file, index=False)
-    # mylist = list(dict.fromkeys(mylist))
 
 
-def main(web_address):
+def prepare_data(web_address):
     starting_web_address = web_address
-    visited_adresses = {starting_web_address}
+    visited_addresses = {starting_web_address}
     LOGGER.info("[Starting]")
     soup = scrape_web(web_address, starting_web_address)
     links = find_links(soup)
 
     text = extract_text(soup.body, web_address)
-    ngram_set = ngram(text, 2)
+    ngram_set = ngram(text, web_address, 2)
     find_email(text)
 
     LOGGER.info("Initial scraping done. Scraping links")
-    for idx, val in enumerate(links):
-        if val in visited_adresses:
+    print("Initial scraping done. Scraping links")
+    for idx, new_address in enumerate(links):
+        print(f"step:{idx+1}/{len(links)} {round(idx+1/len(links))}%")
+        if new_address in visited_addresses:
             LOGGER.debug("Address already visited. Skipping.")
             continue
         else:
-            visited_adresses.add(val)
-            LOGGER.debug(f"Scraping: {val}")
-            soup = scrape_web(val, starting_web_address)
-            text = extract_text(soup.body, val)
+            visited_addresses.add(new_address)
+            LOGGER.debug(f"Scraping: {new_address}")
+            soup = scrape_web(new_address, starting_web_address)
+            text = extract_text(soup.body, new_address)
+            ngram_set = ngram(text, new_address, 2)
             find_email(text)
 
     remove_duplicates()
-    LOGGER.info("[DONE]")
+    LOGGER.info("[DONE] prepare data")
+
+
+def read_ngrams_csv(path_to_file='export/ngrams.csv'):
+    ngrams = []
+    with open(path_to_file, newline='') as csvfile:
+        file = csv.reader(csvfile, delimiter=';')
+        for row in file:
+            ngrams.append(row)
+
+    return ngrams
+
+
+
+
+def jaccard_index(ngrams, url):
+    def return_index(e):
+        return e['jaccard_index']
+    for idx, ngram in enumerate(ngrams):
+        if ngram[0] == url:
+            my_ngrams = set(ngrams.pop(idx)[1:])
+            break
+    try:
+        all_results = []
+        for idx, ngram in enumerate(ngrams):
+            x = set(ngram[1:])
+            all = len(x.union(my_ngrams))
+            common = len(x & my_ngrams)
+            index = common / all
+            all_results.append({'website': ngram[0], 'jaccard_index': index})
+    except:
+        LOGGER.error(f"url not in database")
+        exit(1)
+
+    all_results.sort(reverse=True, key=return_index)
+    
+    return all_results[1:4]
+
+
+def bag_of_words():
+    pass
+
+
+def cosinus_distance():
+    pass
+
+
+def find_similar_webpages(url):
+    ngrams = read_ngrams_csv()
+    top_matches = jaccard_index(ngrams, url)
+    return top_matches
 
 
 if __name__ == "__main__":
-    main(starting_web_address)
+    prepare_data('https://www.techsterowniki.pl/serwis/kontakt-serwis')
+    top_matches = find_similar_webpages('https://www.techsterowniki.pl/k/sterowniki-do-instalacji')
+    LOGGER.info(f"Top matches:{top_matches}")
